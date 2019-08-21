c55d2147db28dff24e5dc13b87f247a4b006d77b
SeRa <SeRa@DESKTOP-VASFCQS>
2019-08-21 11:16:37 +0900

    init

---
+++ visualization/src/test/java/com/sera/example/demo/VisualizationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sera.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VisualizationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
